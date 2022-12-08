494dcb5db33024ccba08ada7959fd580422f2395
Sandesh Bothra <sandesh128@gmail.com>
2022-12-08 13:19:48 +0530

    Added reaction icon for Hamberger menu and Hamberger menu created for the app

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth } from "./../../hooks/useAuth";
+import {
+  AiOutlineMenuUnfold,
+  AiOutlineMenuFold,
+  AiFillHome,
+} from "react-icons/ai";
+import { TbDeviceAnalytics, TbLogout } from "react-icons/tb";
 import "./MenuBar.css";
 
 export const MenuBar = () => {
   const { logout } = useAuth();
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
 
   return (
     <div className="Menubar">
-      <ul className="MenuItems">
-        <li className="MenuItem">Home</li>
-        <li className="MenuItem">Reports</li>
-        <li className="MenuItem" onClick={logout}>
-          Logout
+      <ul className={`MenuItems ${navbarOpen ? "showMenu" : ""}`}>
+        <li className="MenuItem hambergerIcon" onClick={handleToggle}>
+          {navbarOpen ? (
+            <AiOutlineMenuFold className="MenuIcon" />
+          ) : (
+            <AiOutlineMenuUnfold className="MenuIcon" />
+          )}
+        </li>
+        <li className="MenuItem" title="Home">
+          <AiFillHome className="MenuIcon" />
+          {navbarOpen && <p className="MenuText">Home</p>}
+        </li>
+        <li className="MenuItem" title="Reports">
+          <TbDeviceAnalytics className="MenuIcon" />
+          {navbarOpen && <p className="MenuText">Reports</p>}
+        </li>
+        <li className="MenuItem" onClick={logout} title="Logout">
+          <TbLogout className="MenuIcon" />
+          {navbarOpen && <p className="MenuText">Logout</p>}
         </li>
       </ul>
     </div>
